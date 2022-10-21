7ec4e0ed613f4be56ec3869dbcc854ecc032f349
m1st-gh <93106448+m1st-gh@users.noreply.github.com>
2022-10-21 16:17:53 -0400

    added input

--- src/testSuit.java
+++ src/testSuit.java
@@ -10,7 +10,9 @@ public class testSuit {
         Bridges bridges = new Bridges(0, "patelku2", "873014073775");
         bridges.setTitle("");
         bridges.setDescription("");
-        BinTreeElement<String> bintree = Project5.buildParseTree("( ( 72 * 34 ) / ( 8.93 / 3323.99 ) ) ");
+        System.out.println("Please input a SPACE seperated parenthesised equation, i.e. ( ( 8 ) / ( 2 * (2 + 2) )");
+        String input = System.console().readLine();
+        BinTreeElement<String> bintree = Project5.buildParseTree(input);
         bridges.setDataStructure(bintree);
         bridges.visualize();
         System.out.println(Project5.evaluate(bintree));
